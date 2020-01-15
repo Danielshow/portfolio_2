9fc5cad30278d195b25a249176808d6c43d5f99f
danielshow <danielshotonwa53@gmail.com>
2020-01-15 20:33:07 +0100

    add content to home page

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,4 @@
 import React from "react"
-import { Link } from "gatsby"
 
 import Layout from "../components/layout"
 import Image from "../components/image"
@@ -7,14 +6,49 @@ import SEO from "../components/seo"
 
 const IndexPage = () => (
   <Layout>
-    <SEO title="Home" />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-      <Image />
+    <SEO title="Home | Danielshow" />
+    <div className="wrapper">
+      <div className="box social">
+        <div className="social__links">
+          <h1>Social Links</h1>
+          <p>Github</p>
+          <p>LinkedIn</p>
+          <p>Twitter</p>
+          <p>Dev.to</p>
+          <p>Medium</p>
+          <p></p>
+        </div>
+      </div>
+      <div className="box home">
+        <div class="home__nav">
+          <ul class="home__nav--list">
+            <li>Blog</li>
+            <li>About</li>
+            <li>Projects</li>
+          </ul>
+        </div>
+        <div class="home__hero">
+          <h1>Danielshow</h1>
+          <h3>Software Developer</h3>
+          <p>stack: Python, javascript, Ruby</p>
+        </div>
+        <div>
+          Built with gatsby ❤️
+        </div>
+      </div>
+      <div className="box pics">
+        <Image className="pics__img"/>
+      </div>
+      <div className="box about">
+        <h1>About Me</h1>
+        <p>Hello! I’m a Technophile and Software Developer specializing in developing web applications, testing and making the internet a more beautiful and positive place. I am a fast learner who loves learning new concepts and technology. I hold a National Diploma in Computer Engineering from Yaba College of Technology. I have over 3 years of experience in software development.</p>
+      </div>
+      <div className="box d">Specialises</div>
+      <div className="box e">
+        <div className="talk">T</div>
+        <div className="form">F</div>
+      </div>
     </div>
-    <Link to="/page-2/">Go to page 2</Link>
   </Layout>
 )
 
