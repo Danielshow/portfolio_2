0c73aee79ed28e953670d27cfff880fe50b0b0fa
danielshow <danielshotonwa53@gmail.com>
2020-01-31 18:24:28 +0100

    add 4 cards and excerpt

--- src/pages/blog.js
+++ src/pages/blog.js
@@ -17,6 +17,7 @@ const Blog = () => {
             title
             date
             slug
+            author
             image {
               childImageSharp {
                 fluid(maxWidth: 300) {
@@ -24,7 +25,9 @@ const Blog = () => {
                 }
               }
             }
-          }
+          },
+          excerpt(pruneLength: 50)
+          timeToRead
         }
       }
     }
@@ -35,6 +38,7 @@ const Blog = () => {
     <Layout>
       <SEO title="All blogs page" />
       <div className="all__blogs">
+        <Link to="/">Home</Link>
         <h1 className="all__blogs--title">All Blogs</h1>
           <section class="cards">
           { nodes.map(node => {
@@ -52,20 +56,21 @@ const Blog = () => {
                     <path
                       d="M12,20A7,7 0 0,1 5,13A7,7 0 0,1 12,6A7,7 0 0,1 19,13A7,7 0 0,1 12,20M19.03,7.39L20.45,5.97C20,5.46 19.55,5 19.04,4.56L17.62,6C16.07,4.74 14.12,4 12,4A9,9 0 0,0 3,13A9,9 0 0,0 12,22C17,22 21,17.97 21,13C21,10.88 20.26,8.93 19.03,7.39M11,14H13V8H11M15,1H9V3H15V1Z"
                     /></svg
-                  ><span class="card__time">15 min</span>
+                  ><span class="card__time">{node.timeToRead} min</span>
                 </div>
               </div>
               <div class="card__img" style={{ backgroundImage: `url(${node.frontmatter.image.childImageSharp.fluid.src})`}}></div>
               <a href="#" class="card_link">
               
-              <div class="card__img--hover" style={{ backgroundImage: `url(${node.frontmatter.image.childImageSharp.fluid.src})`}}></div>
+                <div class="card__img--hover" style={{ backgroundImage: `url(${node.frontmatter.image.childImageSharp.fluid.src})`}}></div>
               </a>
               <div class="card__info">
                 <span class="card__category"> Recipe</span>
-                <h3 class="card__title">Crisp Spanish tortilla Matzo brei</h3>
+                <p>{node.excerpt}</p>
+                <h3 class="card__title"> { node.frontmatter.title }</h3>
                 <span class="card__by"
                   >by
-                  <a href="#" class="card__author" title="author">Celeste Mills</a></span
+                  <a href="#" class="card__author" title="author"> { node.frontmatter.author }</a></span
                 >
               </div>
             </article>
